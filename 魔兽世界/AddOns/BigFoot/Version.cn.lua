3cb42fcad59811cf89672ca5227605af4356677e
GodlikeLPC <godlikelpc@126.com>
2019-09-25 10:15:21 +0800

    日更

---
+++ 魔兽世界/AddOns/BigFoot/Version.cn.lua
@@ -0,0 +1,105 @@
+if GetLocale()~='zhCN' then return end
+local main= "1.13.2."
+local minor = "16"
+BIGFOOT_VERSION = "zhCN"..main..minor;
+
+function GetMinorVersion()
+	return minor
+end
+
+BigFootChangelog_ah();
+
+	BigFootChangelog_at("2019/09/24 (1.13.2.16)")
+	BigFootChangelog_ar("大脚设置","添加触发类技能动作条高亮开关。")
+	BigFootChangelog_ar("职业助手","恢复猎人助手及部分功能。")
+	BigFootChangelog_ar("大脚插件","同步更新整合类插件。")
+
+
+	BigFootChangelog_at("2019/09/20 (1.13.2.15)")
+	BigFootChangelog_ar("大脚设置","添加自动下马开关。")
+	BigFootChangelog_ar("鼠标提示","修复已知问题。")
+	BigFootChangelog_ar("大脚插件","同步更新整合类插件。")
+
+
+	BigFootChangelog_at("2019/09/18 (1.13.2.14)")
+	BigFootChangelog_ar("大脚插件","修复与优化(感谢NGA网友-hu60)。")
+
+
+	BigFootChangelog_at("2019/09/17 (1.13.2.13)")
+	BigFootChangelog_ar("大脚插件","修改基础库结构，现在整合库为单独插件。")
+	BigFootChangelog_ar("大脚插件","修复部分已知问题(感谢NGA网友-hu60)。")
+	BigFootChangelog_ar("大脚插件","同步更新整合类插件。")
+	BigFootChangelog_ar("大脚插件","添加距离显示(RangeDisplay)。")
+	BigFootChangelog_ar("大脚插件","添加天赋模拟器(alaTalentEmu)。")
+	BigFootChangelog_ar("大脚插件","添加地图NPC提示(HandyNotes_NPCs)。")
+	BigFootChangelog_ar("大脚插件","添加飞艇提示(HandyNotes_TravelGuide)。")
+
+
+	BigFootChangelog_at("2019/09/12 (1.13.2.12)")
+	BigFootChangelog_ar("任务助手","尝试修复卡顿问题(感谢NGA网友-hu60)。")
+	BigFootChangelog_ar("真实血量","升级至最新版本(2.10)。")
+	BigFootChangelog_ar("目标施法提示","升级至最新版本(1.0.5)。")
+
+
+	BigFootChangelog_at("2019/09/11 (1.13.2.11)")
+	BigFootChangelog_ar("首领报警","修复无法正常加载的问题。")
+
+
+	BigFootChangelog_at("2019/09/11 (1.13.2.10)")
+	BigFootChangelog_ar("大脚插件","优化部分插件代码逻辑(感谢NGA网友-hu60)。")
+	BigFootChangelog_ar("大脚插件","添加飞行点提示(HandyNotes_FlightMasters)。")
+
+
+	BigFootChangelog_at("2019/09/10 (1.13.2.9)")
+	BigFootChangelog_ar("大脚插件","同步更新整合类插件。")
+	BigFootChangelog_ar("大脚插件","添加一键施法(Clique)。")
+	BigFootChangelog_ar("大脚插件","添加地图标记(HandyNotes)。")
+	BigFootChangelog_ar("大脚插件","添加真实血量(RealMobHealth)。")
+
+
+	BigFootChangelog_at("2019/09/04 (1.13.2.8)")
+	BigFootChangelog_ar("大脚插件","优化文件结构。")
+
+
+	BigFootChangelog_at("2019/09/04 (1.13.2.7)")
+	BigFootChangelog_ar("大脚插件","修改部分整合插件的默认配置。")
+	BigFootChangelog_ar("界面调整","升级至最新版本(4.8.5)。")
+	BigFootChangelog_ar("掉落查询","升级至最新版本(1.1.8)。")
+	BigFootChangelog_ar("首领报警","升级至最新版本(1.13.5)。")
+
+
+	BigFootChangelog_at("2019/08/30 (1.13.2.6)")
+	BigFootChangelog_ar("大脚插件","添加副本掉落查询(AtlasLootClassic)。")
+
+
+	BigFootChangelog_at("2019/08/30 (1.13.2.5)")
+	BigFootChangelog_ar("大脚插件","添加目标施法条增强(ClassicCastbars)。")
+	BigFootChangelog_ar("大脚插件","修改部分整合插件的默认配置。")
+	BigFootChangelog_ar("首领报警","升级至最新版本(1.13.3)。")
+	BigFootChangelog_ar("伤害统计","升级至最新版本(1.13.2b)。")
+
+
+	BigFootChangelog_at("2019/08/28 (1.13.2.4)")
+	BigFootChangelog_ar("大脚插件","修复部分已知问题。")
+
+
+	BigFootChangelog_at("2019/08/27 (1.13.2.3)")
+	BigFootChangelog_ar("大脚插件","修复部分已知问题。")
+	BigFootChangelog_ar("大脚插件","添加计时条插件。")
+	BigFootChangelog_ar("大脚插件","添加任务追踪增强。")
+	BigFootChangelog_ar("任务助手","修改默认配置,不再自动交接任务。")
+
+
+	BigFootChangelog_at("2019/08/27 (1.13.2.2)")
+	BigFootChangelog_ar("大脚插件","修复部分已知问题，感谢反馈。")
+	BigFootChangelog_ar("任务助手","修改默认配置。")
+
+
+	BigFootChangelog_at("2019/08/26 (1.13.2.1)")
+	BigFootChangelog_ar("大脚插件","经典旧世初版，反馈请移步NGA相关板块。")
+
+
+BigFootChangelog_af();
+
+
+BF_VERSION_CHECKSUM = "55"
